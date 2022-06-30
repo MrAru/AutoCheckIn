87abb74658946e26f3497a309e2c2d54ab472d98
Aru <mraru@outlook.com>
2022-06-30 10:18:26 +0800

    Update

--- report.py
+++ report.py
@@ -1,52 +1,34 @@
 import asyncio
-import subprocess
 import sys
+import time
 
 import requests
 from playwright.async_api import async_playwright
 
-# ENV = {
-# "BOTID": sys.argv[1],
-# "CHATID": sys.argv[2]
-# }
-
-
-def notice(title, message):
-    print(message)
-    subprocess.Popen(['notify-send', title, message])
+ENV = {
+    "BOTID": sys.argv[1],
+    "CHATID": sys.argv[2],
+    "USERNAME": sys.argv[3],
+    "PASSWD": sys.argv[4],
+}
 
 
 async def main(headless=True):
-    cookies = [{
-        "name": "SAAS_S_ID",
-        "value": "xmu",
-        "domain": "xmuxg.xmu.edu.cn",
-        "path": "/"
-    }, {
-        "name": "SAAS_U",
-        "value": "6a20ba0fd410d55eedbaa22f13983a3e186ed1c5",
-        "domain": "xmuxg.xmu.edu.cn",
-        "path": "/"
-    }, {
-        "name": "iPlanetDirectoryPro",
-        "value": "AQIC5wM2LY4Sfcxa5PLjr8Uvj%2BRcCqotLTcCXT7MlwUnphU%3D%40AAJTSQACMDE%3D%23",
-        "domain": ".xmu.edu.cn",
-        "path": "/"
-    }, {
-        "name": "JSESSIONID",
-        "value": "B4ACA5ECAC17DED28FFDD3B79CDFB6D3",
-        "domain": "xmuxg.xmu.edu.cn",
-        "path": "/"
-    }]
     async with async_playwright() as p:
-        brower = await p.chromium.launch(headless=headless)
+        brower = await p.firefox.launch(headless=headless)
         context = await brower.new_context()
-        await context.add_cookies(cookies)
         page = await context.new_page()
-        await page.goto("http://xmuxg.xmu.edu.cn/xmu/app/214")
+        await page.goto(
+            "https://ids.xmu.edu.cn/authserver/login?service=https://xmuxg.xmu.edu.cn/login/cas/xmu"
+        )
+        await page.fill("#username", ENV["USERNAME"])
+        await page.fill("#password", ENV["PASSWD"])
+        await page.click(".auth_login_btn")
+        await page.route("*qq*", lambda route: route.abort())
+        await page.goto("https://xmuxg.xmu.edu.cn/app/214")
         await page.click('[title="我的表单"]')
         await page.click('//*[@id="select_1582538939790"]/div/div')
-        await page.click('//html/body/div[8]/ul/div')
+        await page.click("//html/body/div[8]/ul/div")
         page.on("dialog", lambda dialog: dialog.accept())
         await page.click('[class="form-save position-absolute"]')
         await asyncio.sleep(3)
@@ -56,23 +38,32 @@ async def main(headless=True):
         html = await page.query_selector('[title="是 Yes"]')
         text = await html.inner_text()
         if text == "是 Yes":
-            title = "Success"
+            res = True
             message = f"打卡成功 / 今日已打卡\n返回值: {text}"
         else:
-            title = "Failure"
+            res = False
             message = f"打卡失败 / 未到打卡时间\n返回值: {text}"
-        notice(title, message)
-        # requests.get(f"https://api.telegram.org/bot{ENV['BOTID']}/sendMessage?chat_id={ENV['CHATID']}&parse_mode=Markdown&text={message}")
+        requests.get(
+            f"https://api.telegram.org/bot{ENV['BOTID']}/sendMessage?chat_id={ENV['CHATID']}&parse_mode=Markdown&text={message}"
+        )
+        return res
+
+
+def run():
+    try:
+        res = asyncio.run(main())
+    except Exception as e:
+        res = False
+        message = f"打卡失败 程序报错\n```\n{e}\n```"
+        requests.get(
+            f"https://api.telegram.org/bot{ENV['BOTID']}/sendMessage?chat_id={ENV['CHATID']}&parse_mode=Markdown&text={message}"
+        )
+    return res
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        headless = eval(sys.argv[1])
-    else:
-        headless = True
-    try:
-        asyncio.run(main(headless=headless))
-    except Exception as e:
-        message = f"打卡失败 程序报错\n```\n{e}\n```"
-        notice("Error", message)
-        # requests.get(f"https://api.telegram.org/bot{ENV['BOTID']}/sendMessage?chat_id={ENV['CHATID']}&parse_mode=Markdown&text={message}")
+    for _ in range(3):
+        if run():
+            quit()
+        else:
+            time.sleep(20)
